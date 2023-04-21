643c060f17d522b3584dc49de4703c90057ddff2
prateekvarma <prtk.varma@gmail.com>
2023-04-21 12:33:38 -0600

    Added icons to buttons

--- src/components/ChooseAndPlay.tsx
+++ src/components/ChooseAndPlay.tsx
@@ -1,13 +1,19 @@
-import styles from './ChooseAndPlay.module.css' 
+import styles from './ChooseAndPlay.module.css';
 import HandSelection from './HandSelection';
+
+import {
+  FaRegHandPaper,
+  FaRegHandRock,
+  FaRegHandScissors,
+} from 'react-icons/fa';
 
 const ChooseAndPlay = () => {
   return (
     <>
       <div className={styles.choiceBtnCtn}>
-        <HandSelection name="Rock" />
-        <HandSelection name="Paper" />
-        <HandSelection name="Scissors" />
+        <HandSelection name='Rock' icon={<FaRegHandRock size={60} />} />
+        <HandSelection name='Paper' icon={<FaRegHandPaper size={60} />} />
+        <HandSelection name='Scissors' icon={<FaRegHandScissors size={60} />} />
       </div>
       <button className={styles.playBtn}>Play</button>
     </>
--- src/components/HandSelection.tsx
+++ src/components/HandSelection.tsx
@@ -2,12 +2,13 @@ import styles from './HandSelection.module.css';
 
 interface Props {
   name: string;
+  icon: JSX.Element
 }
 
-const HandSelection: React.FC<Props> = ({ name }) => {
+const HandSelection: React.FC<Props> = ({ name, icon }) => {
   return (
     <>
-      <button className={styles.choiceBtn}>{name}</button>
+      <button className={styles.choiceBtn}>{name} {icon}</button>
     </>
   );
 };
