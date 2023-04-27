fafb1bde1e4e44d54db26200d4cf372f5639d2f8
prateekvarma <prtk.varma@gmail.com>
2023-04-27 14:31:38 -0600

    Click event on selecting hand  triggers Reducer

--- src/components/ChooseAndPlay.tsx
+++ src/components/ChooseAndPlay.tsx
@@ -2,17 +2,18 @@ import { useOptions } from '../context/optionsContext';
 import styles from './ChooseAndPlay.module.css';
 import HandSelection from './HandSelection';
 
-import {
-  FaRegHandPaper,
-  FaRegHandRock,
-  FaRegHandScissors,
-} from 'react-icons/fa';
-
 const ChooseAndPlay = () => {
   const optionsContext = useOptions();
 
-  const handOptionsArray = optionsContext.options.map((item) => {
-    return <HandSelection name={item.name} icon={item.icon} />;
+  const handOptionsArray = optionsContext.options.map((hand, i) => {
+    return (
+      <HandSelection
+        key={i}
+        name={hand.name}
+        icon={hand.icon}
+        handChoiceIndex={i}
+      />
+    );
   });
 
   return (
--- src/components/HandSelection.tsx
+++ src/components/HandSelection.tsx
@@ -1,14 +1,28 @@
+import { useOptions } from '../context/optionsContext';
+import { OptionActionKind } from '../reducers/scoreReducerTypes';
 import styles from './HandSelection.module.css';
 
 interface Props {
   name: string;
-  icon: JSX.Element
+  icon: JSX.Element;
+  handChoiceIndex: number;
 }
 
-const HandSelection: React.FC<Props> = ({ name, icon }) => {
+const HandSelection: React.FC<Props> = ({ name, icon, handChoiceIndex }) => {
+  const optionsContext = useOptions();
+  const { dispatch, state } = optionsContext;
+  const selectOption = (index: number) => {
+    dispatch({ type: OptionActionKind.UPDATE_PLAYER_CHOICE, payload: index });
+  };
+
   return (
     <>
-      <button className={styles.choiceBtn}>{name} {icon}</button>
+      <button
+        className={styles.choiceBtn}
+        onClick={() => selectOption(handChoiceIndex)}
+      >
+        {name} {icon}
+      </button>
     </>
   );
 };
