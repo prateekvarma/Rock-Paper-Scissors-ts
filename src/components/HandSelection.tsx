1fbd10cd5f5742310caf1946e8a0caf074c67af7
prateekvarma <prtk.varma@gmail.com>
2023-04-28 14:50:32 -0600

    Added css active class on button click

--- src/components/HandSelection.tsx
+++ src/components/HandSelection.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useOptions } from '../context/optionsContext';
 import { OptionActionKind } from '../reducers/scoreReducerTypes';
 import styles from './HandSelection.module.css';
@@ -9,16 +10,24 @@ interface Props {
 }
 
 const HandSelection: React.FC<Props> = ({ name, icon, handChoiceIndex }) => {
+  const [handPressed, setHandPressed] = useState<boolean>(false);
   const optionsContext = useOptions();
   const { dispatch, state } = optionsContext;
+  const selectedHandIndex = state.playerHand;
+
   const selectOption = (index: number) => {
     dispatch({ type: OptionActionKind.UPDATE_PLAYER_CHOICE, payload: index });
+    setHandPressed(true);
   };
 
   return (
     <>
       <button
-        className={styles.choiceBtn}
+        className={`${styles.choiceBtn} ${
+          handPressed && handChoiceIndex === selectedHandIndex
+            ? styles.activeChoice
+            : ''
+        }`}
         onClick={() => selectOption(handChoiceIndex)}
       >
         {name} {icon}
