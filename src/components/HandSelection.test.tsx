553a5bb05aea5fbba32eab777aa1318740efbbb4
prateekvarma <prtk.varma@gmail.com>
2023-05-12 14:44:26 -0600

    Test to check if css class is added after clicking button

--- src/components/HandSelection.test.tsx
+++ src/components/HandSelection.test.tsx
@@ -2,6 +2,17 @@ import { render, screen } from '@testing-library/react';
 import { describe, it, vi } from 'vitest';
 import HandSelection from './HandSelection';
 import { FaRegHandPaper } from 'react-icons/fa';
+import userEvent from '@testing-library/user-event';
+import { OptionsProvider } from '../context/optionsContext';
+
+vi.mock('./HandSelection.module.css', () => {
+  return {
+    default: {
+      choiceButton: 'choiceBtn',
+      activeChoice: 'activeChoice',
+    },
+  };
+});
 
 describe('Hand Selection', () => {
   it('should render the hand selection component with the right props', () => {
@@ -14,9 +25,29 @@ describe('Hand Selection', () => {
     );
 
     const hand = screen.getByText(/paper/i);
-    const icon = screen.getByTestId('paper')
+    const icon = screen.getByTestId('paper');
 
     expect(hand).toBeInTheDocument();
-    expect(icon).toBeVisible()
+    expect(icon).toBeVisible();
+  });
+
+  it('should display active css class after clicking', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <OptionsProvider>
+        <HandSelection
+          name='paper'
+          icon={<FaRegHandPaper data-testid='paper' />}
+          handChoiceIndex={2}
+        />
+      </OptionsProvider>
+    );
+
+    const hand = screen.getByText(/paper/i);
+
+    await user.click(hand);
+
+    expect(hand).toHaveClass('activeChoice');
   });
 });
